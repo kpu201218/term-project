3d07e262ec5792a634d9fab0981d8438dd856c98
kpu201218 <kpu201218@naver.com>
2016-06-12 21:35:22 +0900

    라이브러리 패키징 업데이트

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='movie DB',
+    version='0.1',
+    py_modules=['Movie_DB_startup','statics','sendmail',
+                'inputs','xmlParse'],
+    )
